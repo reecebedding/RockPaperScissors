9be2df432af34dbf9c2b68c432016ae3e1c18c1a
Reece Bedding <reece.bedding@digital.voa.gov.uk>
2017-06-28 11:34:05 +0100

    Added game looping logic with user input parsing

--- src/com/reecebedding/Main.java
+++ src/com/reecebedding/Main.java
@@ -1,9 +1,27 @@
 package com.reecebedding;
 
+import java.util.InvalidPropertiesFormatException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Rock paper scissors");
+        Scanner scanner = new Scanner(System.in);
 
+        while(true){
+
+            Choices.Option userOption;
+
+            try {
+                String userChoice = scanner.nextLine();
+                userOption = Choices.GetOptionFromChoice(userChoice);
+            } catch (InvalidPropertiesFormatException exn){
+                System.out.println("Invalid choice.");
+                continue;
+            }
+
+            System.out.println(userOption.GetName());
+        }
     }
 }
